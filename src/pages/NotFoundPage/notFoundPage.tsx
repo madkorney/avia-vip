9eb0a373c20a55d16a59ebb8bab0a17c0e70b4f8
Sergey Kornienko <madkorney@gmail.com>
2022-11-16 02:01:04 +0300

    refactor: update NotFound component, change imports, add styles

--- src/pages/NotFoundPage/notFoundPage.tsx
+++ src/pages/NotFoundPage/notFoundPage.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 
+import './notFoundPage.scss';
+
 const NotFoundPage = () => {
   return (
-    <div>
+    <div className="not-found">
       <p>Запрашиваемая страница не найдена.. :( </p>
       <div>
         Вернуться <Link to="/avia">к началу</Link>?
