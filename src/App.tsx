d484ff44abae1a7879d950baa4d95f77eb9403fd
Sergey Kornienko <madkorney@gmail.com>
2022-11-16 01:59:59 +0300

    refactor: remove extra components, fix styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
-import TemplatePage from 'pages/TemplatePage/templatePage';
-import SearchFlightsPage from 'pages/SearchFlightsPage/searchFlightsPage';
-import FoundFlightsPage from 'pages/FoundFlightsPage/foundFlightsPage';
-import NotFoundPage from 'pages/NotFoundPage/notFoundPage';
+import SearchFlightsPage from 'pages/SearchFlightsPage';
+import FoundFlightsPage from 'pages/FoundFlightsPage';
+import NotFoundPage from 'pages/NotFoundPage';
 
-import {
-  REACT_APP_BASENAME as BASENAME,
-  FLIGHTS_DATA,
-  FlightData,
-  FlightSearchParams,
-  INIT_SEARCH_PARAMS,
-  checkFlightFitsSearch,
-} from 'data';
+import { REACT_APP_BASENAME as BASENAME, FlightSearchParams, INIT_SEARCH_PARAMS } from 'data';
 
 import './App.scss';
 
 const App = () => {
-  const [flightsFilteredList, setFlightsFilteredList] = useState<FlightData[]>([]);
   const [searchParams, setSearchParams] = useState<FlightSearchParams>(INIT_SEARCH_PARAMS);
 
   const searchHandler = (params: FlightSearchParams) => {
@@ -27,29 +18,14 @@ const App = () => {
     });
   };
 
-  useEffect(() => {
-    const updatedFligthsList = FLIGHTS_DATA.filter((flight) => {
-      checkFlightFitsSearch(flight, searchParams);
-    });
-    setFlightsFilteredList(() => {
-      return updatedFligthsList;
-    });
-  }, [searchParams]);
-
   return (
     <BrowserRouter basename={BASENAME}>
       <Routes>
-        <Route path="/avia" element={<TemplatePage />}>
-          <Route
-            path="/avia"
-            element={<SearchFlightsPage handler={searchHandler} params={searchParams} />}
-          />
-          <Route
-            path="/avia/info"
-            element={<FoundFlightsPage flightsList={flightsFilteredList} />}
-          />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
+        <Route
+          path="/avia"
+          element={<SearchFlightsPage handler={searchHandler} params={searchParams} />}
+        />
+        <Route path="/avia/info" element={<FoundFlightsPage params={searchParams} />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
