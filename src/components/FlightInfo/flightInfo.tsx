47f21031d632605030fc2207bf7992be11a22bb9
Sergey Kornienko <madkorney@gmail.com>
2022-11-16 02:07:44 +0300

    feat: implement FlightInfo component with styles

--- src/components/FlightInfo/flightInfo.tsx
+++ src/components/FlightInfo/flightInfo.tsx
@@ -1,16 +1,51 @@
-// import './header.scss';
+import PriceTag from './PrceTag';
+import FileSegment from './FlightSegment';
 
-import { FlightData } from 'data';
+import { FlightsSet } from 'data';
+
+import './flightInfo.scss';
+import { Fragment } from 'react';
 
 type FlightInfoProps = {
-  flight: FlightData;
+  oneWay: boolean;
+  flightsSetTo: FlightsSet;
+  flightsSetBack?: FlightsSet;
+  dateTo: string;
+  dateBack?: string;
+  departureCity: string;
+  arrivalCity: string;
 };
 
 const FlightInfo = (props: FlightInfoProps) => {
-  const { flight } = props;
+  const { oneWay, flightsSetTo, flightsSetBack, dateTo, dateBack, departureCity, arrivalCity } =
+    props;
   return (
-    <div>
-      <p>Flight number : {flight.flightNumber}</p>
+    <div className="flightset-container">
+      <div className="flight-segments-container">
+        <FileSegment
+          flightsSet={flightsSetTo}
+          date={dateTo}
+          departureCity={departureCity}
+          arrivalCity={arrivalCity}
+        />
+
+        {oneWay ? null : (
+          <Fragment>
+            <div className="dot-line"></div>
+            <FileSegment
+              flightsSet={flightsSetBack!}
+              date={dateBack!}
+              departureCity={arrivalCity}
+              arrivalCity={departureCity}
+            />
+          </Fragment>
+        )}
+      </div>
+      {oneWay ? (
+        <PriceTag amount={flightsSetTo.priceRUB} />
+      ) : (
+        <PriceTag amount={flightsSetTo.priceRUB + flightsSetBack!.priceRUB} />
+      )}
     </div>
   );
 };
