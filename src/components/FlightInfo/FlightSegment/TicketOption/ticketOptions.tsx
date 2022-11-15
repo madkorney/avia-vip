eb09f40e489c8af175c0969c9ac1d830cdb7760a
Sergey Kornienko <madkorney@gmail.com>
2022-11-16 02:06:01 +0300

    feat: implement TicketOption component with styles

---
+++ src/components/FlightInfo/FlightSegment/TicketOption/ticketOptions.tsx
@@ -0,0 +1,10 @@
+import './ticketOption.scss';
+const TicketOptions = (props: { option: string }) => {
+  return (
+    <div className="refund-option-container">
+      <div>{props.option}</div>
+    </div>
+  );
+};
+
+export default TicketOptions;
