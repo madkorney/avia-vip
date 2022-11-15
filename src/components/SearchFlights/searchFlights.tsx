679c40cde2f86fcfd6143ecf4dbe6240b088ef71
Sergey Kornienko <madkorney@gmail.com>
2022-11-16 02:08:30 +0300

    feat: implement SearchFlights component

--- src/components/SearchFlights/searchFlights.tsx
+++ src/components/SearchFlights/searchFlights.tsx
@@ -1,24 +1,117 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { SearchProps } from 'data';
+import Button from '@mui/material/Button';
 
-// import './searchFlight.scss';
+import { SearchProps, FlightSearchParams } from 'data';
+import './searchFlight.scss';
 
 const SearchFlights = (props: SearchProps) => {
-  const { params, handler } = props;
-  const [] = useState(params);
+  const [cityFrom, setCityFrom] = useState('');
+  const [cityTo, setCityTo] = useState('');
+  const [dateTo, setDateTo] = useState(new Date().toLocaleDateString('ru-RU'));
+  const [dateBack, setDateBack] = useState('');
+  const [isValidated, setIsValidated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let validated = true;
+    validated = validated && !!cityTo;
+    validated = validated && !!cityFrom;
+    validated = validated && !!dateTo;
+    validated = validated && !!dateTo.match(/[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/i);
+    if (dateBack) {
+      validated = validated && !!dateBack.match(/[0-9]{2}\.[0-9]{2}\.[0-9]{4}$/i);
+    }
+    setIsValidated(validated);
+  }, [cityTo, cityFrom, dateTo, dateBack]);
+
+  const handleClick = () => {
+    const newParams: FlightSearchParams = {
+      oneWay: !dateBack,
+      departureCity: cityFrom,
+      arrivalCity: cityTo,
+      departureDate: dateTo,
+    };
+    if (dateBack) {
+      newParams.returnDate = dateBack;
+    }
+    props.handler(newParams);
+    navigate('info');
+  };
 
   return (
     <div className="seacrh-form-container">
-      <p>search flights..</p>
+      <form className="inputs-container">
+        <label>
+          Откуда
+          <input
+            type="text"
+            name="city-from"
+            id="city-from"
+            placeholder="Город вылета"
+            required
+            value={cityFrom}
+            onChange={(e) => {
+              setCityFrom(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Куда
+          <input
+            type="text"
+            name="city-to"
+            id="city-to"
+            placeholder="Город прилета"
+            required
+            value={cityTo}
+            onChange={(e) => {
+              setCityTo(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Tуда
+          <input
+            className={dateTo ? 'date-input-active' : 'date-input-inactive'}
+            type="text"
+            name="date-to"
+            id="date-to"
+            placeholder="дд.мм.гг"
+            required
+            value={dateTo}
+            onChange={(e) => {
+              setDateTo(e.target.value);
+            }}
+          />
+        </label>
+        <label>
+          Обратно
+          <input
+            className={dateBack ? 'date-input-active' : 'date-input-inactive'}
+            type="text"
+            name="date-back"
+            id="date-back"
+            placeholder="дд.мм.гг"
+            value={dateBack}
+            onChange={(e) => {
+              setDateBack(e.target.value);
+            }}
+          />
+        </label>
+        <div className="dline"></div>
+      </form>
 
-      <button
-        onClick={() => {
-          handler(params);
-        }}
-      >
-        submit
-      </button>
+      <div className="btn-container">
+        <Button variant="contained" onClick={handleClick} disabled={!isValidated}>
+          Найти билеты
+        </Button>
+      </div>
+
+      {/* <Button variant="contained" disabled>
+        Disabled
+      </Button> */}
     </div>
   );
 };
